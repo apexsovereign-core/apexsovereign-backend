0f89e08a1a44239accd5ae987656d02b0669323a
Anton John Intes <antonjohnintes7@gmail.com>
2026-09-17 01:36:20 +0800

    Update compute_broker.py

--- compute_broker.py
+++ compute_broker.py
@@ -39,11 +39,11 @@ def require_env(name: str) -> str:
     return value
 
 
-DATABASE_URL = require_env("postgresql://postgres.eeclrffbjbnapsajmtqn:Kodakksaint777@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres")
+DATABASE_URL = require_env("ostgresql://postgres.eeclrffbjbnapsajmtqn:Kodakksaint777@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres")
 PAYPAL_CLIENT_ID = require_env("BAAnJ3a3oIIe5LKdWQwr10uR8Uc4nayYYlfkHNtaTcJhZD5E5QQo9ULhoBQ5eCYB24P1LJL3VTltrNLaE8")
-PAYPAL_CLIENT_SECRET = require_env("EDze29dnVH0Bgmz29XmpgavSEXv7OxbKP6ziB-QqOndkgnNo-ntTytBQmjATri6zPVAFGl3C5J1uC6Ci)
+PAYPAL_CLIENT_SECRET = require_env("EDze29dnVH0Bgmz29XmpgavSEXv7OxbKP6ziB-QqOndkgnNo-ntTytBQmjATri6zPVAFGl3C5J1uC6Ci")
 PAYPAL_WEBHOOK_ID = require_env("0GS90368KN5946222")
-APP_SECRET_API_KEY = require_env("1c10d58092b5049a42e960e0f55eb29f")
+APP_SECRET_API_KEY = require_env("A1c10d58092b5049a42e960e0f55eb29f")
 
 PAYPAL_API_BASE = os.getenv("https://api-m.paypal.com")
 
